04b64a6ab809464f7ecdfdec38543843df361cd1
jeppekorgaard <korg@itu.dk>
2021-10-01 13:58:39 +0200

    added enum to string converter

--- Assignment4.Entities/KanbanContext.cs
+++ Assignment4.Entities/KanbanContext.cs
@@ -12,5 +12,13 @@ namespace Assignment4.Entities
 
         public KanbanContext(DbContextOptions<KanbanContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Task>()
+                .Property(e => e.State)
+                .HasConversion(new EnumToStringConverter<State>());
+        }
+
     }
 }
