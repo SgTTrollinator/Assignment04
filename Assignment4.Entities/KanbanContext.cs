bcdd2c23b016e6cecf58405abb74cdb719e409d3
jeppekorgaard <korg@itu.dk>
2021-10-01 13:56:56 +0200

    created entities

--- Assignment4.Entities/KanbanContext.cs
+++ Assignment4.Entities/KanbanContext.cs
@@ -1,6 +1,16 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
 namespace Assignment4.Entities
 {
-    public class KanbanContext
+    public class KanbanContext : DbContext
     {
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public KanbanContext(DbContextOptions<KanbanContext> options) : base(options) { }
+
     }
 }
